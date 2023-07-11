5f67fa0a364122709fd8e84313c7dca075e6d2a5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-11 18:03:44 +0300

    add assets index

---
+++ src/assets/index.js
@@ -0,0 +1,199 @@
+import monitors from './images/monitors.webp';
+import flyingBananas from './images/flying-bananas.webp';
+import roomWithSoftShadows from './images/room-with-soft-shadows.webp';
+import tshirtConfigurator from './images/tshirt-configurator.webp';
+import caustics from './images/caustics.webp';
+import volumetricLightGodray from './images/volumetric-light-godray.webp';
+import enterPortals from './images/enter-portals.webp';
+import passThroughPortals from './images/pass-through-portals.webp';
+import magicBox from './images/magic-box.webp';
+import glassFlower from './images/glass-flower.webp';
+import imageGallery from './images/image-gallery.webp';
+import horizontalTiles from './images/horizontal-tiles.webp';
+import bestServedBoldChristmasBaubles from './images/best-served-bold-christmas-baubles.webp';
+import theThreeGraces from './images/the-three-graces.webp';
+import frostedGlass from './images/frosted-glass.webp';
+import gltfjsx400kbDrone from './images/gltfjsx-400kb-drone.webp';
+import starWars from './images/star-wars.webp';
+import brunoSimons20kChallenge from './images/bruno-simons-20k-challenge.webp';
+import scrollcontrolsAndLensRefraction from './images/scrollcontrols-and-lens-refraction.webp';
+import buildingDynamicEnvmaps from './images/building-dynamic-envmaps.webp';
+import nextjsPrism from './images/nextjs-prism.webp';
+import buildingLiveEnvmaps from './images/building-live-envmaps.webp';
+import shoeConfigurator from './images/shoe-configurator.webp';
+import audioAnalyser from './images/audio-analyser.webp';
+import groundReflectionsAndVideoTextures from './images/ground-reflections-and-video-textures.webp';
+import threejsJourneyLv1 from './images/threejs-journey-lv1.webp';
+import threejsJourneyPortal from './images/threejs-journey-portal.webp';
+import mixingHtmlAndWebglWithOcclusion from './images/mixing-html-and-webgl-w-occlusion.webp';
+import interactiveSplineSceneLiveHtml from './images/interactive-spline-scene-live-html.webp';
+import diamondRefraction from './images/diamond-refraction.webp';
+import diamondRing from './images/diamond-ring.webp';
+import envmapGroundProjection from './images/envmap-ground-projection.webp';
+import splineGlassShapes from './images/spline-glass-shapes.webp';
+import csgBunnyUsegroups from './images/csg-bunny-usegroups.webp';
+import csgHouse from './images/csg-house.webp';
+import gltfAnimationsTiedToScroll from './images/gltf-animations-tied-to-scroll.webp';
+import objectClump from './images/object-clump.webp';
+import htmlInputFields from './images/html-input-fields.webp';
+import useintersectAndScrollcontrols from './images/useintersect-and-scrollcontrols.webp';
+import infiniteScroll from './images/infinite-scroll.webp';
+import scrollcontrolsWithMinimap from './images/scrollcontrols-with-minimap.webp';
+import instancedParticlesEffects from './images/instanced-particles-effects.webp';
+import dbismutFurniture from './images/dbismut-furniture.webp';
+import portalShapes from './images/portal-shapes.webp';
+import aquarium from './images/aquarium.webp';
+import portals from './images/portals.webp';
+import interEpoxyResin from './images/inter-epoxy-resin.webp';
+import stagePresetsGltfjsx from './images/stage-presets-gltfjsx.webp';
+import reactEllipsecurve from './images/react-ellipsecurve.webp';
+import iridescentDecals from './images/iridescent-decals.webp';
+import bakingSoftShadows from './images/baking-soft-shadows.webp';
+import ssrTest from './images/ssr-test.webp';
+import csgOperationsRapierPhysics from './images/csg-operations-rapier-physics.webp';
+import faucetsSelectHighlight from './images/faucets-select-highlight.webp';
+import rapierPhysics from './images/rapier-physics.webp';
+import bloomHdrWorkflowGltf from './images/bloom-hdr-workflow-gltf.webp';
+import groundProjectedEnvmapsLamina from './images/ground-projected-envmaps-lamina.webp';
+import basicBallpit from './images/basic-ballpit.webp';
+import backdropAndCables from './images/backdrop-and-cables.webp';
+import clones from './images/clones.webp';
+import lamina1x from './images/lamina-1x.webp';
+import reactPpOutlines from './images/react-pp-outlines.webp';
+import gatsbyStars from './images/gatsby-stars.webp';
+import pmndrsVercel from './images/pmndrs-vercel.webp';
+import sportHall from './images/sport-hall.webp';
+import nightTrain from './images/night-train.webp';
+import bouncyWatch from './images/bouncy-watch.webp';
+import transparentAesopBottles from './images/transparent-aesop-bottles.webp';
+import raycastCycling from './images/raycast-cycling.webp';
+import landingPage from './images/landing-page.webp';
+import scrollcontrolsGltf from './images/scrollcontrols-gltf.webp';
+import mergedInstance from './images/merged-instance.webp';
+import gpgpuCurlNoiseDof from './images/gpgpu-curl-noise-dof.webp';
+import hiKeyBubbles from './images/hi-key-bubbles.webp';
+import floatingInstancedShoes from './images/floating-instanced-shoes.webp';
+import simpleAudioAnalyser from './images/simple-audio-analyser.webp';
+import cameraScroll from './images/camera-scroll.webp';
+import springyBoxes from './images/springy-boxes.webp';
+import floatingDiamonds from './images/floating-diamonds.webp';
+import gltfAnimations from './images/gltf-animations.webp';
+import sparksAndEffects from './images/sparks-and-effects.webp';
+import cameraShake from './images/camera-shake.webp';
+import ragdollPhysics from './images/ragdoll-physics.webp';
+import reactSpringAnimations from './images/react-spring-animations.webp';
+import mountTransitions from './images/mount-transitions.webp';
+import floatingLaptop from './images/floating-laptop.webp';
+import zustandSite from './images/zustand-site.webp';
+import cellFracture from './images/cell-fracture.webp';
+import routerTransitions from './images/router-transitions.webp';
+import softShadows from './images/soft-shadows.webp';
+import lulabyCity from './images/lulaby-city.webp';
+import vikingShip from './images/viking-ship.webp';
+import wobblingSphere from './images/wobbling-sphere.webp';
+import moksha from './images/moksha.webp';
+import flexboxYogaInWebgl from './images/flexbox-yoga-in-webgl.webp';
+import confetti from './images/confetti.webp';
+import learnWithJason from './images/learn-with-jason.webp';
+import volumetricSpotlight from './images/volumetric-spotlight.webp';
+
+export {
+  monitors,
+  flyingBananas,
+  roomWithSoftShadows,
+  tshirtConfigurator,
+  caustics,
+  volumetricLightGodray,
+  enterPortals,
+  passThroughPortals,
+  magicBox,
+  glassFlower,
+  imageGallery,
+  horizontalTiles,
+  bestServedBoldChristmasBaubles,
+  theThreeGraces,
+  frostedGlass,
+  gltfjsx400kbDrone,
+  starWars,
+  brunoSimons20kChallenge,
+  scrollcontrolsAndLensRefraction,
+  buildingDynamicEnvmaps,
+  nextjsPrism,
+  buildingLiveEnvmaps,
+  shoeConfigurator,
+  audioAnalyser,
+  groundReflectionsAndVideoTextures,
+  threejsJourneyLv1,
+  threejsJourneyPortal,
+  mixingHtmlAndWebglWithOcclusion,
+  interactiveSplineSceneLiveHtml,
+  diamondRefraction,
+  diamondRing,
+  envmapGroundProjection,
+  splineGlassShapes,
+  csgBunnyUsegroups,
+  csgHouse,
+  gltfAnimationsTiedToScroll,
+  objectClump,
+  htmlInputFields,
+  useintersectAndScrollcontrols,
+  infiniteScroll,
+  scrollcontrolsWithMinimap,
+  instancedParticlesEffects,
+  dbismutFurniture,
+  portalShapes,
+  aquarium,
+  portals,
+  interEpoxyResin,
+  stagePresetsGltfjsx,
+  reactEllipsecurve,
+  iridescentDecals,
+  bakingSoftShadows,
+  ssrTest,
+  csgOperationsRapierPhysics,
+  faucetsSelectHighlight,
+  rapierPhysics,
+  bloomHdrWorkflowGltf,
+  groundProjectedEnvmapsLamina,
+  basicBallpit,
+  backdropAndCables,
+  clones,
+  lamina1x,
+  reactPpOutlines,
+  gatsbyStars,
+  pmndrsVercel,
+  sportHall,
+  nightTrain,
+  bouncyWatch,
+  transparentAesopBottles,
+  raycastCycling,
+  landingPage,
+  scrollcontrolsGltf,
+  mergedInstance,
+  gpgpuCurlNoiseDof,
+  hiKeyBubbles,
+  floatingInstancedShoes,
+  simpleAudioAnalyser,
+  cameraScroll,
+  springyBoxes,
+  floatingDiamonds,
+  gltfAnimations,
+  sparksAndEffects,
+  cameraShake,
+  ragdollPhysics,
+  reactSpringAnimations,
+  mountTransitions,
+  floatingLaptop,
+  zustandSite,
+  cellFracture,
+  routerTransitions,
+  softShadows,
+  lulabyCity,
+  vikingShip,
+  wobblingSphere,
+  moksha,
+  flexboxYogaInWebgl,
+  confetti,
+  learnWithJason,
+  volumetricSpotlight,
+};
