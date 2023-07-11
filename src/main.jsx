82853228a220662f29a5487cca6c698d867ee977
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-11 23:13:51 +0300

    format

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
 
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AnimatePresence>
-      <App />
-    </AnimatePresence>
+    <App />
   </BrowserRouter>
 );
