77b84a8d259236ad0b2070a483a42b68d4faaece
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-11 22:26:46 +0300

    reactify carousel (use hooks)

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { styled } from 'styled-components';
 
 import { CarouselCard } from './CarouselCard';
@@ -6,109 +6,91 @@ import { dataStore } from '../../store/dataStore';
 
 export const Carousel = () => {
   const data = dataStore((state) => state.data);
-  const cardSpacing = 10;
+  const [progress, setProgress] = useState(2),
+    [startX, setStartX] = useState(0),
+    [active, setActive] = useState(0),
+    [isDown, setIsDown] = useState(false);
+  const speedWheel = 0.02,
+    speedDrag = -0.1,
+    gap = 10;
 
-  useLayoutEffect(() => {
-    /*--------------------
-    Vars
-    --------------------*/
-    let progress = 1;
-    let startX = 0;
-    let active = 0;
-    let isDown = false;
+  const getZindex = (array, index) => array.map((_, i) => (index === i ? array.length : array.length - Math.abs(index - i)));
 
-    /*--------------------
-    Contants
-    --------------------*/
-    const speedWheel = 0.02;
-    const speedDrag = -0.1;
+  const $items = document.querySelectorAll('.carousel-item');
 
-    /*--------------------
-    Get Z
-    --------------------*/
-    const getZindex = (array, index) => array.map((_, i) => (index === i ? array.length : array.length - Math.abs(index - i)));
+  const displayItems = (item, index, active) => {
+    const zIndex = getZindex([...$items], active)[index];
+    item.style.setProperty('--zIndex', zIndex);
+    item.style.setProperty('--active', (index - active) / $items.length * gap);
+  };
 
-    /*--------------------
-    Items
-    --------------------*/
-    const $items = document.querySelectorAll('.carousel-item');
-    const $cursors = document.querySelectorAll('.cursor');
+  const animate = () => {
+    setProgress((prev) => Math.max(0, Math.min(prev, 98)));
+    setActive(Math.floor((progress / 100) * ($items.length - 1)));
+    $items.forEach((item, index) => displayItems(item, index, active));
+  };
 
-    const displayItems = (item, index, active) => {
-      const zIndex = getZindex([...$items], active)[index];
-      item.style.setProperty('--zIndex', zIndex);
-      item.style.setProperty('--active', ((index - active) / $items.length) * cardSpacing);
-    };
-
-    /*--------------------
-    Animate
-    --------------------*/
-    const animate = () => {
-      progress = Math.max(0, Math.min(progress, 100));
-      active = Math.floor((progress / 100) * ($items.length - 1));
-
-      $items.forEach((item, index) => displayItems(item, index, active));
-    };
+  const handleWheel = (e) => {
+    const wheelProgress = e.deltaY * speedWheel;
+    setProgress((prev) => prev + wheelProgress);
     animate();
+  };
 
-    /*--------------------
-    Click on Items
-    --------------------*/
-    $items.forEach((item, i) => {
-      item.addEventListener('click', () => {
-        progress = (i / $items.length) * 100 + 10;
-        animate();
-      });
-    });
+  const handleMouseMove = (e) => {
+    // TODO: move to app maybe? its the animated cursor
+    // if (e.type === 'mousemove') {
+    //   console.log('cursors', $cursors);
+    //   $cursors.forEach(($cursor) => {
+    //     $cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    //   });
+    // }
+    if (!isDown) return;
+    const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+    const mouseProgress = (x - startX) * speedDrag;
+    setProgress((prev) => prev + mouseProgress);
+    setStartX(x);
+    animate();
+  };
 
-    /*--------------------
-    Handlers
-    --------------------*/
-    const handleWheel = (e) => {
-      const wheelProgress = e.deltaY * speedWheel;
-      progress = progress + wheelProgress;
-      animate();
-    };
+  const handleMouseDown = (e) => {
+    setIsDown(true);
+    setStartX(e.clientX || (e.touches && e.touches[0].clientX) || 0);
+  };
 
-    const handleMouseMove = (e) => {
-      if (e.type === 'mousemove') {
-        $cursors.forEach(($cursor) => {
-          $cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-        });
-      }
-      if (!isDown) return;
-      const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
-      const mouseProgress = (x - startX) * speedDrag;
-      progress = progress + mouseProgress;
-      startX = x;
-      animate();
-    };
+  const handleMouseUp = () => {
+    setIsDown(false);
+  };
 
-    const handleMouseDown = (e) => {
-      isDown = true;
-      startX = e.clientX || (e.touches && e.touches[0].clientX) || 0;
-    };
+  const handleClick = (index) => {
+    setProgress(index);
+    animate();
+  };
 
-    const handleMouseUp = () => {
-      isDown = false;
-    };
-
-    /*--------------------
-    Listeners
-    --------------------*/
+  useEffect(() => {
+    animate();
     document.addEventListener('mousewheel', handleWheel);
-    document.addEventListener('mousedown', handleMouseDown);
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-    document.addEventListener('touchstart', handleMouseDown);
-    document.addEventListener('touchmove', handleMouseMove);
-    document.addEventListener('touchend', handleMouseUp);
+    return () => { document.removeEventListener('mousewheel', handleWheel); };
   }, []);
+
+  useEffect(() => {
+    animate();
+  }, [active, progress, startX, isDown]);
 
   return (
     <Container className='carousel'>
       {data.map((ex, index) => (
-        <CarouselCard key={`carousel-card-${index}`} index={index + 1} {...ex} />
+        <CarouselCard
+          key={`carousel-card-${index}`}
+          index={index}
+          {...ex}
+          onClick={() => handleClick(index)}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          onTouchStart={handleMouseDown}
+          onTouchMove={handleMouseMove}
+          onTouchEnd={handleMouseUp}
+        />
       ))}
     </Container>
   );
