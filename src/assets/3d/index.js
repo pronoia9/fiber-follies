c9f477e9f59fa014cd253b3c25f21ceca1c57b25
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-12 22:52:53 +0300

    add index to 3d

---
+++ src/assets/3d/index.js
@@ -0,0 +1,199 @@
+// import monitors from './monitors.glb';
+// import flyingBananas from './flying-bananas.glb';
+// import roomWithSoftShadows from './room-with-soft-shadows.glb';
+// import tshirtConfigurator from './tshirt-configurator.glb';
+// import caustics from './caustics.glb';
+// import volumetricLightGodray from './volumetric-light-godray.glb';
+// import enterPortals from './enter-portals.glb';
+// import passThroughPortals from './pass-through-portals.glb';
+// import magicBox from './magic-box.glb';
+// import glassFlower from './glass-flower.glb';
+// import imageGallery from './image-gallery.glb';
+// import horizontalTiles from './horizontal-tiles.glb';
+// import bestServedBoldChristmasBaubles from './best-served-bold-christmas-baubles.glb';
+// import theThreeGraces from './the-three-graces.glb';
+// import frostedGlass from './frosted-glass.glb';
+// import gltfjsx400kbDrone from './gltfjsx-400kb-drone.glb';
+// import starWars from './star-wars.glb';
+// import brunoSimons20kChallenge from './bruno-simons-20k-challenge.glb';
+// import scrollcontrolsAndLensRefraction from './scrollcontrols-and-lens-refraction.glb';
+// import buildingDynamicEnvmaps from './building-dynamic-envmaps.glb';
+// import nextjsPrism from './nextjs-prism.glb';
+// import buildingLiveEnvmaps from './building-live-envmaps.glb';
+// import shoeConfigurator from './shoe-configurator.glb';
+// import audioAnalyser from './audio-analyser.glb';
+// import groundReflectionsAndVideoTextures from './ground-reflections-and-video-textures.glb';
+import threejsJourneyLv1 from './threejs-journey-lv1.glb';
+// import threejsJourneyPortal from './threejs-journey-portal.glb';
+// import mixingHtmlAndWebglWithOcclusion from './mixing-html-and-webgl-w-occlusion.glb';
+// import interactiveSplineSceneLiveHtml from './interactive-spline-scene-live-html.glb';
+// import diamondRefraction from './diamond-refraction.glb';
+// import diamondRing from './diamond-ring.glb';
+// import envmapGroundProjection from './envmap-ground-projection.glb';
+// import splineGlassShapes from './spline-glass-shapes.glb';
+// import csgBunnyUsegroups from './csg-bunny-usegroups.glb';
+// import csgHouse from './csg-house.glb';
+// import gltfAnimationsTiedToScroll from './gltf-animations-tied-to-scroll.glb';
+// import objectClump from './object-clump.glb';
+// import htmlInputFields from './html-input-fields.glb';
+// import useintersectAndScrollcontrols from './useintersect-and-scrollcontrols.glb';
+// import infiniteScroll from './infinite-scroll.glb';
+// import scrollcontrolsWithMinimap from './scrollcontrols-with-minimap.glb';
+// import instancedParticlesEffects from './instanced-particles-effects.glb';
+// import dbismutFurniture from './dbismut-furniture.glb';
+// import portalShapes from './portal-shapes.glb';
+// import aquarium from './aquarium.glb';
+// import portals from './portals.glb';
+// import interEpoxyResin from './inter-epoxy-resin.glb';
+// import stagePresetsGltfjsx from './stage-presets-gltfjsx.glb';
+// import reactEllipsecurve from './react-ellipsecurve.glb';
+// import iridescentDecals from './iridescent-decals.glb';
+// import bakingSoftShadows from './baking-soft-shadows.glb';
+// import ssrTest from './ssr-test.glb';
+// import csgOperationsRapierPhysics from './csg-operations-rapier-physics.glb';
+// import faucetsSelectHighlight from './faucets-select-highlight.glb';
+// import rapierPhysics from './rapier-physics.glb';
+// import bloomHdrWorkflowGltf from './bloom-hdr-workflow-gltf.glb';
+// import groundProjectedEnvmapsLamina from './ground-projected-envmaps-lamina.glb';
+// import basicBallpit from './basic-ballpit.glb';
+// import backdropAndCables from './backdrop-and-cables.glb';
+// import clones from './clones.glb';
+// import lamina1x from './lamina-1x.glb';
+// import reactPpOutlines from './react-pp-outlines.glb';
+// import gatsbyStars from './gatsby-stars.glb';
+// import pmndrsVercel from './pmndrs-vercel.glb';
+// import sportHall from './sport-hall.glb';
+// import nightTrain from './night-train.glb';
+// import bouncyWatch from './bouncy-watch.glb';
+// import transparentAesopBottles from './transparent-aesop-bottles.glb';
+// import raycastCycling from './raycast-cycling.glb';
+// import landingPage from './landing-page.glb';
+// import scrollcontrolsGltf from './scrollcontrols-gltf.glb';
+// import mergedInstance from './merged-instance.glb';
+// import gpgpuCurlNoiseDof from './gpgpu-curl-noise-dof.glb';
+// import hiKeyBubbles from './hi-key-bubbles.glb';
+// import floatingInstancedShoes from './floating-instanced-shoes.glb';
+// import simpleAudioAnalyser from './simple-audio-analyser.glb';
+// import cameraScroll from './camera-scroll.glb';
+// import springyBoxes from './springy-boxes.glb';
+// import floatingDiamonds from './floating-diamonds.glb';
+// import gltfAnimations from './gltf-animations.glb';
+// import sparksAndEffects from './sparks-and-effects.glb';
+// import cameraShake from './camera-shake.glb';
+// import ragdollPhysics from './ragdoll-physics.glb';
+// import reactSpringAnimations from './react-spring-animations.glb';
+// import mountTransitions from './mount-transitions.glb';
+// import floatingLaptop from './floating-laptop.glb';
+// import zustandSite from './zustand-site.glb';
+// import cellFracture from './cell-fracture.glb';
+// import routerTransitions from './router-transitions.glb';
+// import softShadows from './soft-shadows.glb';
+// import lulabyCity from './lulaby-city.glb';
+// import vikingShip from './viking-ship.glb';
+// import wobblingSphere from './wobbling-sphere.glb';
+// import moksha from './moksha.glb';
+// import flexboxYogaInWebgl from './flexbox-yoga-in-webgl.glb';
+// import confetti from './confetti.glb';
+// import learnWithJason from './learn-with-jason.glb';
+// import volumetricSpotlight from './volumetric-spotlight.glb';
+
+export {
+  // monitors,
+  // flyingBananas,
+  // roomWithSoftShadows,
+  // tshirtConfigurator,
+  // caustics,
+  // volumetricLightGodray,
+  // enterPortals,
+  // passThroughPortals,
+  // magicBox,
+  // glassFlower,
+  // imageGallery,
+  // horizontalTiles,
+  // bestServedBoldChristmasBaubles,
+  // theThreeGraces,
+  // frostedGlass,
+  // gltfjsx400kbDrone,
+  // starWars,
+  // brunoSimons20kChallenge,
+  // scrollcontrolsAndLensRefraction,
+  // buildingDynamicEnvmaps,
+  // nextjsPrism,
+  // buildingLiveEnvmaps,
+  // shoeConfigurator,
+  // audioAnalyser,
+  // groundReflectionsAndVideoTextures,
+  threejsJourneyLv1,
+  // threejsJourneyPortal,
+  // mixingHtmlAndWebglWithOcclusion,
+  // interactiveSplineSceneLiveHtml,
+  // diamondRefraction,
+  // diamondRing,
+  // envmapGroundProjection,
+  // splineGlassShapes,
+  // csgBunnyUsegroups,
+  // csgHouse,
+  // gltfAnimationsTiedToScroll,
+  // objectClump,
+  // htmlInputFields,
+  // useintersectAndScrollcontrols,
+  // infiniteScroll,
+  // scrollcontrolsWithMinimap,
+  // instancedParticlesEffects,
+  // dbismutFurniture,
+  // portalShapes,
+  // aquarium,
+  // portals,
+  // interEpoxyResin,
+  // stagePresetsGltfjsx,
+  // reactEllipsecurve,
+  // iridescentDecals,
+  // bakingSoftShadows,
+  // ssrTest,
+  // csgOperationsRapierPhysics,
+  // faucetsSelectHighlight,
+  // rapierPhysics,
+  // bloomHdrWorkflowGltf,
+  // groundProjectedEnvmapsLamina,
+  // basicBallpit,
+  // backdropAndCables,
+  // clones,
+  // lamina1x,
+  // reactPpOutlines,
+  // gatsbyStars,
+  // pmndrsVercel,
+  // sportHall,
+  // nightTrain,
+  // bouncyWatch,
+  // transparentAesopBottles,
+  // raycastCycling,
+  // landingPage,
+  // scrollcontrolsGltf,
+  // mergedInstance,
+  // gpgpuCurlNoiseDof,
+  // hiKeyBubbles,
+  // floatingInstancedShoes,
+  // simpleAudioAnalyser,
+  // cameraScroll,
+  // springyBoxes,
+  // floatingDiamonds,
+  // gltfAnimations,
+  // sparksAndEffects,
+  // cameraShake,
+  // ragdollPhysics,
+  // reactSpringAnimations,
+  // mountTransitions,
+  // floatingLaptop,
+  // zustandSite,
+  // cellFracture,
+  // routerTransitions,
+  // softShadows,
+  // lulabyCity,
+  // vikingShip,
+  // wobblingSphere,
+  // moksha,
+  // flexboxYogaInWebgl,
+  // confetti,
+  // learnWithJason,
+  // volumetricSpotlight,
+};
