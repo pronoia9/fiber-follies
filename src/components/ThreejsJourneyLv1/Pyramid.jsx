3cd95bd3382c1f21f1c8b046bbdac7cdb98dd5b9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-13 00:15:34 +0300

    finish pyramid

--- src/components/ThreejsJourneyLv1/Pyramid.jsx
+++ src/components/ThreejsJourneyLv1/Pyramid.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+import { useMatcapTexture } from '@react-three/drei';
+import { useSpring, a } from '@react-spring/three';
+
 export const Pyramid = ({ model }) => {
   const { nodes } = model;
-  
-  return <></>;
+  const [matcap] = useMatcapTexture('489B7A_A0E7D9_6DC5AC_87DAC7', 1024);
+  const [spring, api] = useSpring(() => ({ rotation: [0, 0, 0], config: { mass: 5, tension: 200 } }), []);
+
+  useEffect(() => {
+    let timeout;
+    const rotate = () => {
+      api.start({ rotation: [(Math.random() - 0.5) * Math.PI * 3, 0, (Math.random() - 0.5) * Math.PI * 3] });
+      timeout = setTimeout(rotate, (0.5 + Math.random() * 2) * 1000);
+    };
+    rotate();
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <a.mesh geometry={nodes.Pyramid.geometry} position={[-0.8, 1.33, 0.25]} {...spring}>
+      <meshMatcapMaterial matcap={matcap} />
+    </a.mesh>
+  );
 };
