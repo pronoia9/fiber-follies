d5b1a628aa8b121715f73ad34109e3e6106ccd84
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-13 00:11:43 +0300

    finish cactus

--- src/components/ThreejsJourneyLv1/Cactus.jsx
+++ src/components/ThreejsJourneyLv1/Cactus.jsx
@@ -1,5 +1,11 @@
+import { MeshWobbleMaterial } from '@react-three/drei';
+
 export const Cactus = ({ model }) => {
-  const { nodes } = model;
-  
-  return <></>;
+  const { nodes, materials } = model;
+
+  return (
+    <mesh geometry={nodes.Cactus.geometry} position={[-0.42, 0.51, -0.62]} rotation={[Math.PI / 2, 0, 0]}>
+      <MeshWobbleMaterial factor={0.4} map={materials.Cactus.map} />
+    </mesh>
+  );
 };
