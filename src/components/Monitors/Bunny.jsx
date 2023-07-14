d0223914187eb6ea066011a1357143ff1fab3b64
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 19:20:37 +0300

    fix/add imports

--- src/components/Monitors/Bunny.jsx
+++ src/components/Monitors/Bunny.jsx
@@ -1,3 +1,11 @@
-export const Bunny = () => {
-  return <div>Bunny</div>;
+import { useGLTF } from "@react-three/drei";
+
+export const Bunny = (props) => {
+  const { nodes } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/bunny/model.gltf');
+
+  return (
+    <mesh receiveShadow castShadow geometry={nodes.bunny.geometry} {...props}>
+      <meshStandardMaterial color='#222' roughness={0.5} />
+    </mesh>
+  );
 };
