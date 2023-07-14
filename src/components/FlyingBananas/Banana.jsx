9b734cba9775468804039b3f21d6eba2367fe96e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 21:29:44 +0300

    init flying bananas components

---
+++ src/components/FlyingBananas/Banana.jsx
@@ -0,0 +1,3 @@
+export const Banana = () => {
+  return <></>;
+};
---
+++ src/components/FlyingBananas/index.js
@@ -0,0 +1,7 @@
+import { Banana } from './Banana';
+import { Bananas } from './Bananas';
+import { FlyingBananas } from './FlyingBananas';
+import { Overlay } from './Overlay';
+import { VelvetBanana } from './VelvetBanana';
+
+export { Banana, Bananas, FlyingBananas, Overlay, VelvetBanana };
