5452ded7753e7568c3fe36646e412d2f42e56013
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 18:55:55 +0300

    finish screen interactive

--- src/components/Monitors/ComputersScreenInteractive.jsx
+++ src/components/Monitors/ComputersScreenInteractive.jsx
@@ -1,3 +1,16 @@
-export const ComputersScreenInteractive = () => {
-  return <></>;
+import { PerspectiveCamera } from '@react-three/drei';
+import { Screen, SpinningBox } from '.';
+
+// Renders a monitor with a spinning box
+export const ScreenInteractive = (props) => {
+  return (
+    <Screen {...props}>
+      <PerspectiveCamera makeDefault manual aspect={1 / 1} position={[0, 0, 10]} />
+      <color attach='background' args={['orange']} />
+      <ambientLight intensity={0.2} />
+      <pointLight position={[10, 10, 10]} intensity={0.75} />
+      <pointLight position={[-10, -10, -10]} />
+      <SpinningBox position={[-3.15, 0.75, 0]} scale={0.5} />
+    </Screen>
+  );
 };
