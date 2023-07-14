166e565cc9c307c25fba7d9cb734bdd4ade8def0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 17:37:38 +0300

    init leva

---
+++ src/components/Monitors/Leva.js
@@ -0,0 +1,13 @@
+export const levaProps = {
+  canvas: {
+    shadows: true,
+    dpr: [1, 2]
+  },
+
+  camera: {
+    position: { value: [-1.5, 1, 5.5] },
+    fov: 45,
+    near: 1,
+    far: 20,
+  },
+};
--- src/components/Monitors/index.js
+++ src/components/Monitors/index.js
@@ -3,5 +3,6 @@ import { Monitors } from './Monitors';
 import { Overlay } from './Overlay';
 import { Scene } from './Scene';
 import { SpinningBox } from './SpinningBox';
+import { levaProps } from './Leva';
 
-export { Computers, Monitors, Overlay, Scene, SpinningBox };
+export { Computers, Monitors, Overlay, Scene, SpinningBox, levaProps };
