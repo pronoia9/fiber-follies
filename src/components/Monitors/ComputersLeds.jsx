f0f1f6ac8fcb3a9a5d78c1b8b0c5aee830a7f675
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 18:54:39 +0300

    finish leds

--- src/components/Monitors/ComputersLeds.jsx
+++ src/components/Monitors/ComputersLeds.jsx
@@ -1,3 +1,39 @@
-export const ComputersLeds = () => {
-  return <></>;
+import * as THREE from 'three';
+import { useMemo, useContext, createContext, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useGLTF, Merged, RenderTexture, PerspectiveCamera, Text } from '@react-three/drei';
+import { Screen, ScreenInteractive, ScreenText, SpinningBox } from '.';
+
+// Renders flashing LED's
+export const Leds = ({ instances }) => {
+  const ref = useRef();
+  const { nodes } = useGLTF('/src/components/Monitors/assets/computers_1-transformed.glb');
+
+  useMemo(() => {
+    nodes.Sphere.material = new THREE.MeshBasicMaterial();
+    nodes.Sphere.material.toneMapped = false;
+  }, []);
+
+  useFrame((state) => {
+    ref.current?.children?.forEach((instance) => {
+      const rand = Math.abs(2 + instance.position.x);
+      const t = Math.round((1 + Math.sin(rand * 10000 + state.clock.elapsedTime * rand)) / 2);
+      instance.color.setRGB(0, t * 1.1, t);
+    });
+  });
+
+  return (
+    <group ref={ref}>
+      <instances.Sphere position={[-0.41, 1.1, -2.21]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[0.59, 1.32, -2.22]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[1.77, 1.91, -1.17]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[2.44, 1.1, -0.79]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[4.87, 3.8, -0.1]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[1.93, 3.8, -3.69]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[-2.35, 3.8, -3.48]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[-4.71, 4.59, -1.81]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[-3.03, 2.85, 1.19]} scale={0.005} color={[1, 2, 1]} />
+      <instances.Sphere position={[-1.21, 1.73, -1.49]} scale={0.005} color={[1, 2, 1]} />
+    </group>
+  );
 };
