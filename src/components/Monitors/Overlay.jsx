efd9b11d162d2f5683f93b850cc5070db99de4ce
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 16:59:27 +0300

    finish overlay

--- src/components/Monitors/Overlay.jsx
+++ src/components/Monitors/Overlay.jsx
@@ -1,3 +1,13 @@
 export const Overlay = () => {
-  return <div>Overlay</div>;
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>ok —</div>
+      <a href='https://pmnd.rs/' style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+        pmnd.rs
+        <br />
+        dev collective
+      </a>
+      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>16/12/2022</div>
+    </div>
+  );
 };
