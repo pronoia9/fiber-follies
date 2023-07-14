19c8dd43972dabfb53110cd5d4e8ed3e994d9d44
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 17:54:57 +0300

    init scene components

--- src/components/Monitors/index.js
+++ src/components/Monitors/index.js
@@ -1,9 +1,11 @@
 import { Computers } from './Computers';
+import { Effects } from './Effects';
 import { Instances } from './Instances';
+import { levaProps } from './Leva';
+import { Lights } from './Lights';
 import { Monitors } from './Monitors';
 import { Overlay } from './Overlay';
 import { Scene } from './Scene';
 import { SpinningBox } from './SpinningBox';
-import { levaProps } from './Leva';
 
-export { Computers, Instances, Monitors, Overlay, Scene, SpinningBox, levaProps };
+export { Computers, Effects, Instances, levaProps, Lights, Monitors, Overlay, Scene, SpinningBox };
