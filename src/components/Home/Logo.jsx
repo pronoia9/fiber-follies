00dbf1c827d4ab84de6dfe3a9622988e5d1bf87f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-14 21:47:29 +0300

    update logo styles

--- src/components/Home/Logo.jsx
+++ src/components/Home/Logo.jsx
@@ -5,7 +5,6 @@ import { dataStore } from '../../store/dataStore';
 
 export const Logo = () => {
   const { logoPosition } = dataStore((state) => ({ logoPosition: state.logoPosition, }));
-  
   return (
     <Container to='/' className='logo' $position={logoPosition}>
       <svg width='931' height='925' viewBox='0 0 931 925' fill='none' xmlns='http://www.w3.org/2000/svg'>
@@ -21,31 +20,35 @@ export const Logo = () => {
 
 const Container = styled(Link)`
   display: ${({ $position }) => !$position && 'none'};
-  position: absolute;
-  z-index: 10;
-  ${({ $position }) => $position.includes('top') && css`top: 28px; `}
-  ${({ $position }) => $position.includes('bottom') && css`bottom: 28px; `}
-  ${({ $position }) => $position.includes('right') && css`right: 28px; `}
-  ${({ $position }) => $position.includes('left') && css`left: 28px; `}
-  width: 30px;
-  height: 30px;
-  background: var(--c-font-primary);
-  border-radius: 50%;
-  opacity: 0.5;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  pointer-events: all;
-  overflow: hidden;
-  padding: 0.1em;
-  z-index: 999999;
-
-  svg {
-    width: 20px;
-    height: 20px;
-
-    rect {
-      fill: var(--c-accent-primary);
+  
+  ${({ $position }) => $position && css`
+    position: absolute;
+    z-index: 10;
+    width: 30px;
+    height: 30px;
+    background: var(--c-font-primary);
+    border-radius: 50%;
+    opacity: 0.5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    pointer-events: all;
+    overflow: hidden;
+    padding: 0.1em;
+    z-index: 999999;
+    
+    svg {
+      width: 20px;
+      height: 20px;
+      
+      rect {
+        fill: var(--c-accent-primary);
+      }
     }
-  }
+  `}
+
+  ${({ $position }) => $position?.includes('top') && css`top: 28px; `}
+  ${({ $position }) => $position?.includes('bottom') && css`bottom: 28px; `}
+  ${({ $position }) => $position?.includes('right') && css`right: 28px; `}
+  ${({ $position }) => $position?.includes('left') && css`left: 28px; `}
 `;
