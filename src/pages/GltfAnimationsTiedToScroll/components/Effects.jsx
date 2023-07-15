a13247657cbf7b900f37621afbca1deeaab12c65
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-16 00:01:42 +0300

    finish lights + effects

--- src/pages/GltfAnimationsTiedToScroll/components/Effects.jsx
+++ src/pages/GltfAnimationsTiedToScroll/components/Effects.jsx
@@ -1,3 +1,9 @@
+import { EffectComposer, TiltShift2 } from '@react-three/postprocessing';
+
 export const Effects = () => {
-  return <></>;
+  return (
+    <EffectComposer disableNormalPass multisampling={4}>
+      <TiltShift2 blur={1} />
+    </EffectComposer>
+  );
 };
--- src/pages/GltfAnimationsTiedToScroll/components/Lights.jsx
+++ src/pages/GltfAnimationsTiedToScroll/components/Lights.jsx
@@ -1,3 +1,8 @@
 export const Lights = () => {
-  return <></>;
+  return (
+    <>
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={2} position={[-5, 5, 5]} castShadow shadow-mapSize={2048} shadow-bias={-0.0001} />
+    </>
+  );
 };
