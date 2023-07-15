500f1c2824057ff196a3ea376680d52e5ce95528
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-15 23:57:49 +0300

    add new components

--- src/pages/GltfAnimationsTiedToScroll/components/Scene.jsx
+++ src/pages/GltfAnimationsTiedToScroll/components/Scene.jsx
@@ -3,6 +3,13 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { useGLTF, useAnimations, useScroll, ScrollControls, SoftShadows } from '@react-three/drei';
 import { EffectComposer, TiltShift2 } from '@react-three/postprocessing';
 
+import { Effects, Lights, Model } from '..';
+
 export const Scene = () => {
-  return <></>;
+  return (
+    <Canvas shadows gl={{ antialias: false }} camera={{ position: [1, 0.5, 2.5], fov: 50 }}>
+      <color attach='background' args={['#f0f0f0']} />
+      <fog attach='fog' args={['#f0f0f0', 0, 20]} />
+    </Canvas>
+  );
 };
--- src/pages/GltfAnimationsTiedToScroll/index.js
+++ src/pages/GltfAnimationsTiedToScroll/index.js
@@ -1,5 +1,7 @@
+import { Effects } from './components/Effects';
+import { Lights } from './components/Lights';
 import { Model } from './components/Model';
 import { Scene } from './components/Scene';
 import { GltfAnimationsTiedToScroll } from './GltfAnimationsTiedToScroll';
 
-export { Model, Scene, GltfAnimationsTiedToScroll };
+export { Effects, Lights, Model, Scene, GltfAnimationsTiedToScroll };
