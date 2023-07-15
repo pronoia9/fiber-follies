3391c94ee8a759c8ca4a985781ccf15e688ef133
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-16 00:07:54 +0300

    finish model

--- src/pages/GltfAnimationsTiedToScroll/components/Model.jsx
+++ src/pages/GltfAnimationsTiedToScroll/components/Model.jsx
@@ -1,3 +1,24 @@
-export const Model = () => {
-  return <></>;
+import { useEffect } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useGLTF, useAnimations, useScroll } from '@react-three/drei';
+
+export const Model = (props) => {
+  const scroll = useScroll();
+  const { nodes, materials, animations } = useGLTF('/src/pages/GltfAnimationsTiedToScroll/assets/jump-transformed.glb');
+  const { ref, actions } = useAnimations(animations);
+
+  useEffect(() => {
+    actions.jump.reset().play().paused = true;
+  }, []);
+
+  useFrame(() => {
+    actions.jump.time = actions.jump.getClip().duration * scroll.offset;
+  });
+
+  return (
+    <group ref={ref} {...props}>
+      <primitive object={nodes.mixamorigHips} />
+      <skinnedMesh castShadow receiveShadow geometry={nodes.Ch03.geometry} material={materials.Ch03_Body} skeleton={nodes.Ch03.skeleton} />
+    </group>
+  );
 };
