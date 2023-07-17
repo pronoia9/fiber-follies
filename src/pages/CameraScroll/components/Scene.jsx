ce8f7b405c6cb12c235146d95bf4a3af503c12ed
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-17 21:33:54 +0300

    add init scene

--- src/pages/CameraScroll/components/Scene.jsx
+++ src/pages/CameraScroll/components/Scene.jsx
@@ -1,3 +1,17 @@
-export const Scene = () => {
-  return <></>;
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+
+import { Model } from '..';
+
+export const Scene = ({ scroll }) => {
+  return (
+    <Canvas shadows eventSource={document.getElementById('root')} eventPrefix='client'>
+      <ambientLight intensity={1} />
+      <Suspense fallback={null}>
+        <Model scroll={scroll} />
+        <Environment preset='city' />
+      </Suspense>
+    </Canvas>
+  );
 };
