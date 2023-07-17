50a1316b94cac3baa5ef0842c2bdf620ed8a93f8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-17 21:29:38 +0300

    finish init page

--- src/pages/CameraScroll/CameraScroll.jsx
+++ src/pages/CameraScroll/CameraScroll.jsx
@@ -1,3 +1,30 @@
+import { styled } from 'styled-components';
+
+import { Scene, Overlay } from '.';
+
 export const CameraScroll = () => {
-  return <div>CameraScroll</div>;
+  return <Container>
+    <Scene />
+    <Overlay />
+  </Container>;
 };
+
+const Container = styled.div`
+  overflow: auto;
+  position: fixed;
+  overflow: hidden;
+  overscroll-behavior-y: none;
+  background-color: #101010;
+  background: radial-gradient(circle at bottom center, #212121 0%, #101010 80%);
+  font-family: 'Inter var', sans-serif;
+  -webkit-font-smoothing: antialiased;
+
+  &, .scroll {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    user-select: none;
+    overflow: hidden;
+  }
+`;
