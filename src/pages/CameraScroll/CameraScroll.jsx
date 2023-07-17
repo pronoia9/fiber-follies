2825db3f93d31de8187e423b32517f58f16d8cb3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-17 22:23:51 +0300

    format + fix styles

--- src/pages/CameraScroll/CameraScroll.jsx
+++ src/pages/CameraScroll/CameraScroll.jsx
@@ -4,35 +4,23 @@ import { styled } from 'styled-components';
 import { Scene, Overlay } from '.';
 
 export const CameraScroll = () => {
-  const overlay = useRef(),
-    caption = useRef(),
-    scroll = useRef(0);
+  const overlay = useRef(), caption = useRef(), scroll = useRef(0);
 
   return (
     <Container>
-      <Scene overlay={overlay} caption={caption} scroll={scroll} />
+      <Scene scroll={scroll} />
       <Overlay ref={overlay} caption={caption} scroll={scroll} />
     </Container>
   );
 };
 
 const Container = styled.div`
-  overflow: auto;
   position: fixed;
-  overflow: hidden;
-  overscroll-behavior-y: none;
-  background-color: #101010;
-  background: radial-gradient(circle at bottom center, #212121 0%, #101010 80%);
   font-family: 'Inter var', sans-serif;
   -webkit-font-smoothing: antialiased;
-
-  &,
-  .scroll {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    user-select: none;
-    overflow: hidden;
-  }
+  background-color: #101010;
+  background: radial-gradient(circle at bottom center, #212121 0%, #101010 80%);
+  overflow: hidden;
+  overscroll-behavior-y: none;
+  user-select: none;
 `;
