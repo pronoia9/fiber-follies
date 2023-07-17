a29fa44c570f3231a1d9910bc59ab241abc6e38f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-17 19:55:50 +0300

    remove scene

--- src/pages/ScrollControlsGltf/index.js
+++ src/pages/ScrollControlsGltf/index.js
@@ -1,6 +1,5 @@
 import { Lights } from './components/Lights';
 import { Model } from './components/Model';
-import { Scene } from './components/Scene';
 import { ScrollControlsGltf } from './ScrollControlsGltf';
 
-export { Lights, Model, Scene, ScrollControlsGltf };
+export { Lights, Model, ScrollControlsGltf };
