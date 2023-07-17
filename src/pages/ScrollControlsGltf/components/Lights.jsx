2b3df6d09a679547c96dde18d2c4dc944b33d8b6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-17 19:55:43 +0300

    add lights

--- src/pages/ScrollControlsGltf/components/Lights.jsx
+++ src/pages/ScrollControlsGltf/components/Lights.jsx
@@ -1,3 +1,8 @@
 export const Lights = () => {
-  return <></>;
+  return (
+    <>
+      <ambientLight intensity={0.03} />
+      <spotLight angle={0.14} color='#ffd0d0' penumbra={1} position={[25, 50, -20]} shadow-mapSize={[2048, 2048]} shadow-bias={-0.0001} castShadow />
+    </>
+  );
 };
