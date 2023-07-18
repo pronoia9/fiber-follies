75409216a2b089e34b4c0e396769cab0c99aa6b5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-18 19:33:09 +0300

    add styles

--- src/pages/MixingHtmlAndWebglWithOcclusion/MixingHtmlAndWebglWithOcclusion.jsx
+++ src/pages/MixingHtmlAndWebglWithOcclusion/MixingHtmlAndWebglWithOcclusion.jsx
@@ -10,4 +10,30 @@ export const MixingHtmlAndWebglWithOcclusion = () => {
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  @import url('https://rsms.me/inter/inter.css');
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  font-family: inter;
+  color: black;
+  background: #202025;
+
+  .content {
+    width: 334px;
+    height: 216px;
+    background: #f0f0f0;
+    border-radius: 3px;
+    overflow-y: auto;
+    padding: 0;
+  }
+
+  .wrapper {
+    padding: 10px;
+    width: 668px;
+    height: 432px;
+    transform: scale(0.5);
+    transform-origin: top left;
+  }
+`;
