45204bebc91b12d17c14114db76ac2554be647a5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-18 19:18:56 +0300

    add effects

--- src/pages/MixingHtmlAndWebglWithOcclusion/components/Effects.jsx
+++ src/pages/MixingHtmlAndWebglWithOcclusion/components/Effects.jsx
@@ -1,3 +1,10 @@
+import { ContactShadows, Environment } from "@react-three/drei";
+
 export const Effects = () => {
-  return <></>;
+  return (
+    <>
+      <Environment preset='city' />
+      <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
+    </>
+  );
 };
