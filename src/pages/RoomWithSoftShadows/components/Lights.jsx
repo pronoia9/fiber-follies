bf65f5c63b067fddc5b8bc30295b8d8fc49de6c4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-18 18:36:47 +0300

    add another light

--- src/pages/RoomWithSoftShadows/components/Lights.jsx
+++ src/pages/RoomWithSoftShadows/components/Lights.jsx
@@ -10,10 +10,13 @@ export const Lights = () => {
   });
 
   return (
-    <group ref={ref}>
-      <directionalLight position={[5, 5, -8]} castShadow intensity={5} shadow-mapSize={2048} shadow-bias={-0.001}>
-        <orthographicCamera attach='shadow-camera' args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]} />
-      </directionalLight>
-    </group>
+    <>
+      <ambientLight intensity={0.4} />
+      <group ref={ref}>
+        <directionalLight position={[5, 5, -8]} castShadow intensity={5} shadow-mapSize={2048} shadow-bias={-0.001}>
+          <orthographicCamera attach='shadow-camera' args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]} />
+        </directionalLight>
+      </group>
+    </>
   );
 };
