1288205d77f917e6c251054a9c811709736e8bb5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-18 19:19:05 +0300

    add init scene

--- src/pages/MixingHtmlAndWebglWithOcclusion/components/Scene.jsx
+++ src/pages/MixingHtmlAndWebglWithOcclusion/components/Scene.jsx
@@ -1,3 +1,26 @@
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
+
+import { Effects, Lights, Model } from '..';
+
 export const Scene = () => {
-  return <div>Scene</div>;
+  return (
+    <Canvas camera={{ position: [-5, 0, -15], fov: 55 }}>
+      <Lights />
+      <Effects />
+      <OrbitControls
+        enablePan={false}
+        enableZoom={false}
+        minPolarAngle={Math.PI / 2.2}
+        maxPolarAngle={Math.PI / 2.2}
+        // {...useControls('Orbit Controls', { enablePan: false, enableZoom: false, minPolarAngle: Math.PI / 2.2, maxPolarAngle: Math.PI / 2.2 })}
+      />
+
+      <Suspense fallback={null}>
+        <Model rotation={[0, Math.PI, 0]} position={[0, 1, 0]} />
+      </Suspense>
+    </Canvas>
+  );
 };
