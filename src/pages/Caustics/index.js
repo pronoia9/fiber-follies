cbb3b8c33de30eea914973f3e521ee6421f988ce
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-06 20:33:06 +0300

    init Environment component

--- src/pages/Caustics/index.js
+++ src/pages/Caustics/index.js
@@ -1,6 +1,7 @@
 import { Caustics } from './Caustics';
+import { Environment } from './components/Environment';
 import { Model } from './components/Model';
 import { Overlay } from './components/Overlay';
 import { Scene } from './components/Scene';
 
-export { Caustics, Model, Overlay, Scene };
+export { Caustics, Environment, Model, Overlay, Scene };
