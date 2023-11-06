e57504b6a13a208bae74c78d69f58a7b8fba5edb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-06 21:02:25 +0300

    move scene stuff to scene

--- src/pages/Caustics/Caustics.jsx
+++ src/pages/Caustics/Caustics.jsx
@@ -1,38 +1,15 @@
-import { useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { AccumulativeShadows, PerformanceMonitor, RandomizedLight } from '@react-three/drei';
+import { useEffect } from 'react';
 
-import { Scene, Overlay, Effects } from '.';
+import { Scene, Overlay } from '.';
 import { dataStore } from '../../store/dataStore';
 
 export const Caustics = () => {
-  const [perfSucks, degrade] = useState(false);
-
   const { setLogoPosition } = dataStore((state) => ({ setLogoPosition: state.setLogoPosition }));
   useEffect(() => setLogoPosition('bottom left'), []);
 
   return (
     <>
-      <Canvas
-        shadows
-        dpr={[1, perfSucks ? 1.5 : 2]}
-        eventSource={document.getElementById('root')}
-        eventPrefix='client'
-        camera={{ position: [20, 0.9, 20], fov: 26 }}
-      >
-        <PerformanceMonitor onDecline={() => degrade(true)} />
-        <color attach='background' args={['#f0f0f0']} />
-
-        <group position={[0, -0.5, 0]} rotation={[0, -0.75, 0]}>
-          <Scene />
-          <AccumulativeShadows frames={100} alphaTest={0.85} opacity={0.8} color='red' scale={20} position={[0, -0.005, 0]}>
-            <RandomizedLight amount={8} radius={6} ambient={0.5} intensity={1} position={[-1.5, 2.5, -2.5]} bias={0.001} />
-          </AccumulativeShadows>
-        </group>
-
-        <Effects perfSucks={perfSucks} />
-      </Canvas>
-
+      <Scene />
       <Overlay />
     </>
   );
