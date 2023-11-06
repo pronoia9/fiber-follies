0ab6b61ffbfa1e8b298dbfb5ca7a91a118949f07
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-06 20:29:39 +0300

    add init canvas + setLogoPosition

--- src/pages/Caustics/Caustics.jsx
+++ src/pages/Caustics/Caustics.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { PerformanceMonitor } from '@react-three/drei';
+
 import { Scene, Overlay } from '.';
+import { dataStore } from '../../store/dataStore';
 
 export const Caustics = () => {
+  const [perfSucks, degrade] = useState(false);
+
+  const { setLogoPosition } = dataStore((state) => ({ setLogoPosition: state.setLogoPosition }));
+  useEffect(() => setLogoPosition('bottom left'), []);
+
   return (
     <>
-      <Scene />
+      <Canvas
+        shadows
+        dpr={[1, perfSucks ? 1.5 : 2]}
+        eventSource={document.getElementById('root')}
+        eventPrefix='client'
+        camera={{ position: [20, 0.9, 20], fov: 26 }}
+      >
+        <PerformanceMonitor onDecline={() => degrade(true)} />
+        <color attach='background' args={['#f0f0f0']} />
+
+        <group position={[0, -0.5, 0]} rotation={[0, -0.75, 0]}>
+          <Scene />
+        </group>
+      </Canvas>
+
       <Overlay />
     </>
   );
