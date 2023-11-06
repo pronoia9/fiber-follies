4750cb492ff0c6b781c141bcf6568eaed583cec2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-06 20:45:15 +0300

    finish effects

--- src/pages/Caustics/components/Effects.jsx
+++ src/pages/Caustics/components/Effects.jsx
@@ -1,6 +1,7 @@
-import { useFrame } from '@react-three/fiber';
-import { easing } from 'maath';
 import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Environment, Lightformer } from '@react-three/drei';
+import { easing } from 'maath';
 
 export const Effects = ({ perfSucks }) => {
   const ref = useRef();
@@ -14,5 +15,22 @@ export const Effects = ({ perfSucks }) => {
     }
   });
 
-  return <Environment></Environment>;
+  // Runtime environments can be too slow on some systems, better safe than sorry with PerfMon
+  return (
+    <Environment frames={perfSucks ? 1 : Infinity} preset='city' resolution={256} background blur={0.8}>
+      <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+      <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+      <group rotation={[Math.PI / 2, 1, 0]}>
+        {[2, -2, 2, -4, 2, -5, 2, -9].map((x, i) => (
+          <Lightformer key={i} intensity={1} rotation={[Math.PI / 4, 0, 0]} position={[x, 4, i * 4]} scale={[4, 1, 1]} />
+        ))}
+        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[50, 2, 1]} />
+        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[50, 2, 1]} />
+        <Lightformer intensity={0.5} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[50, 2, 1]} />
+      </group>
+      <group ref={ref}>
+        <Lightformer intensity={5} form='ring' color='red' rotation-y={Math.PI / 2} position={[-5, 2, -1]} scale={[10, 10, 1]} />
+      </group>
+    </Environment>
+  );
 };
