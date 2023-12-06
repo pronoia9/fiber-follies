7f6ca20303dc5cc309abfb76cd8c0e487a0f2b5d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-06 20:51:50 +0300

    init horizontal tiles components

---
+++ src/pages/HorizontalTiles/components/Minimap.jsx
@@ -0,0 +1,3 @@
+export const Minimap = () => {
+  return <div>Minimap</div>;
+};
---
+++ src/pages/HorizontalTiles/index.js
@@ -0,0 +1,7 @@
+import { HorizontalTiles } from './HorizontalTiles';
+import { Item } from './components/Item';
+import { Items } from './components/Items';
+import { Minimap } from './components/Minimap';
+import { Overlay } from './components/Overlay';
+
+export { HorizontalTiles, Item, Items, Minimap, Overlay };
