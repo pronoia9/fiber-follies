d91a6d5a8eda398cabb198937b977cd263790d27
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-06 21:10:14 +0300

    finish items component

--- src/pages/HorizontalTiles/components/Items.jsx
+++ src/pages/HorizontalTiles/components/Items.jsx
@@ -1,3 +1,22 @@
-export const Items = () => {
-  return <></>;
+import { useThree } from '@react-three/fiber';
+import { ScrollControls } from '@react-three/drei';
+import { useSnapshot } from 'valtio';
+
+import { Item, Minimap } from '..';
+
+export const Items = ({ w = 0.7, gap = 0.15, state }) => {
+  const { urls } = useSnapshot(state);
+  const { width } = useThree((state) => state.viewport);
+  const xW = w + gap;
+
+  return (
+    <ScrollControls horizontal damping={0.1} pages={(width - xW + urls.length * xW) / width}>
+      <Minimap />
+      <Scroll>
+        {urls.map((url, index) => (
+          <Item key={`ht-url-#${index}`} index={index} position={[index * xW, 0, 0]} scale={[w, 4, 1]} url={url} />
+        ))}
+      </Scroll>
+    </ScrollControls>
+  );
 };
