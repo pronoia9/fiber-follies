7c5b87a0989372697a2b7498170a420e1aedda52
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-06 21:22:03 +0300

    add init states/funcs/etc

--- src/pages/HorizontalTiles/components/Item.jsx
+++ src/pages/HorizontalTiles/components/Item.jsx
@@ -1,3 +1,21 @@
-export const Item = () => {
+import { useRef, useState } from 'react';
+import { Color } from 'three';
+import { useScroll } from '@react-three/drei';
+import { useSnapshot } from 'valtio';
+
+import { state } from '../utils';
+
+export const Item = ({ index, position, scale, c = new Color(), ...props }) => {
+  const ref = useRef();
+  const scroll = useScroll();
+  const { clicked, urls } = useSnapshot(state);
+  const [hovered, setHovered] = useState(false);
+
+  const click = () => (state.clicked = index === clicked ? null : index);
+
+  const hover = () => setHovered(true);
+
+  const unhover = () => setHovered(false);
+
   return <></>;
 };
