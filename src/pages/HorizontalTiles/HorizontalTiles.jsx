4250b8dfcf6f074b10d9d48369f9ea8d542a7d55
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-06 21:04:20 +0300

    add init component

--- src/pages/HorizontalTiles/HorizontalTiles.jsx
+++ src/pages/HorizontalTiles/HorizontalTiles.jsx
@@ -1,7 +1,23 @@
-import styled from "styled-components";
+import { Canvas } from '@react-three/fiber';
+import { proxy } from 'valtio';
+import styled from 'styled-components';
+
+import { Items, Overlay } from '.';
+
+const state = proxy({
+  clicked: null,
+  urls: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 5, 7, 8, 2, 4, 9, 6].map((u) => `/src/pages/HorizontalTiles/assets/${u}.jpg`),
+});
 
 export const HorizontalTiles = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Canvas gl={{ antialias: false }} dpr={[1, 1.5]} onPointerMissed={() => (state.clicked = null)}>
+        <Items />
+      </Canvas>
+      <Overlay />
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -9,21 +25,10 @@ const Container = styled.div`
     box-sizing: border-box;
   }
 
-  html,
-  body,
-  #root {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
-
-  body {
-    overscroll-behavior: none;
-    background: #151515;
-    color: #a0a0a0;
-    font-family: 'Inter var', sans-serif;
-  }
+  overscroll-behavior: none;
+  background: #151515;
+  color: #a0a0a0;
+  font-family: 'Inter var', sans-serif;
 
   h1 {
     font-size: 30vw;
