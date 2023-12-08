c795d1b1baedfdc1f60761b856610912b53b812f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-08 18:37:03 +0300

    finish frames

--- src/pages/ImageGallery/components/Frames.jsx
+++ src/pages/ImageGallery/components/Frames.jsx
@@ -1,3 +1,41 @@
-export const Frames = () => {
-  return <></>;
+import { useEffect, useRef } from 'react';
+import { Quaternion, Vector3 } from 'three';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+
+import { Frame, GOLDENRATIO, images } from '..';
+
+export const Frames = ({ currentImage, setCurrentImage, q = new Quaternion(), p = new Vector3() }) => {
+  const ref = useRef(),
+    clicked = useRef();
+
+  useEffect(() => {
+    clicked.current = ref.current.getObjectByName(currentImage);
+    if (clicked.current) {
+      clicked.current.parent.updateWorldMatrix(true, true);
+      clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25));
+      clicked.current.parent.getWorldQuaternion(q);
+    } else {
+      p.set(0, 0, 5.5);
+      q.identity();
+    }
+  }, [currentImage]);
+
+  useFrame((state, dt) => {
+    easing.damp3(state.camera.position, p, 0.4, dt);
+    easing.dampQ(state.camera.quaternion, q, 0.4, dt);
+  });
+
+  return (
+    <group
+      ref={ref}
+      onClick={(e) => (
+        e.stopPropagation(), console.log('e.object.name', e.object.name), setCurrentImage(clicked.current === e.object ? null : e.object.name)
+      )}
+      onPointerMissed={() => setCurrentImage(null)}
+      onPointerOver={(e) => console.log('onPointerOver', e.object)}
+    >
+      {images.map((props) => <Frame key={props.url} {...props} /> /* prettier-ignore */)}
+    </group>
+  );
 };
