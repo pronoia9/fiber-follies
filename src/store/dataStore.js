d72bf8379567f09de118680052f35843a72e3156
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-12 08:06:33 +0300

    delete some comments

--- src/store/dataStore.js
+++ src/store/dataStore.js
@@ -18,17 +18,4 @@ export const dataStore = create((set) => ({
   // setActive: (active) => { set({ active }); },
   // progress: 0,
   // setProgress: (progress) => { set({ progress }); },
-  
-  // progress: { showcase: 0, 'game prototypes': 0, 'basic examples': 0 },
-  // active: { showcase: 0, 'game prototypes': 0, 'basic examples': 0 },
-  // setProgress: (progress) => {
-  //   set((state) => {
-  //     let prev = state.progresses;
-  //     prev[state.tab] = progress;
-  //     return prev;
-  //   });
-  // },
-  // setActive: (active) => {
-  //   set((state) => ({ active: { ...state.active, [state.tab]: active } }));
-  // },
 }));
