6b7f248a0a84406a5544c9fc68ee1d02f261133e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-01-12 21:47:35 +0300

    finish carla component

--- src/pages/GroundReflectionsAndVideoTextures/components/Carla.jsx
+++ src/pages/GroundReflectionsAndVideoTextures/components/Carla.jsx
@@ -1,3 +1,7 @@
-export const Carla = () => {
-  return <></>;
+import { useGLTF } from '@react-three/drei';
+
+export const Carla = (props) => {
+  const { scene } = useGLTF('/src/pages/GroundReflectionsAndVideoTextures/assets/carla-draco.glb');
+
+  return <primitive object={scene} {...props} />;
 };
