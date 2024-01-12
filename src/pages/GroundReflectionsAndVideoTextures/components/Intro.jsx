11a910461ee0d6a182e81343ea0efa02ae0a5d0c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-01-12 21:32:03 +0300

    finish camera settings in intro

--- src/pages/GroundReflectionsAndVideoTextures/components/Intro.jsx
+++ src/pages/GroundReflectionsAndVideoTextures/components/Intro.jsx
@@ -1,3 +1,12 @@
+import { useState } from 'react';
+import { Vector3 } from 'three';
+import { useFrame } from '@react-three/fiber';
+
 export const Intro = () => {
-  return <></>;
+  const [vec] = useState(() => new Vector3());
+
+  return useFrame((state) => {
+    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05);
+    state.camera.lookAt(0, 0, 0);
+  });
 };
