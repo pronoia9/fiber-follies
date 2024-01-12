4aa5195b34061db67ba28d1e37e076049623f61f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-01-12 21:38:13 +0300

    finish init ground component

--- src/pages/GroundReflectionsAndVideoTextures/components/Ground.jsx
+++ src/pages/GroundReflectionsAndVideoTextures/components/Ground.jsx
@@ -1,3 +1,22 @@
+import { Reflector, useTexture } from '@react-three/drei';
+
 export const Ground = () => {
-  return <></>;
+  const [floor, normal] = useTexture([
+    '/pages/GroundReflectionsAndVideoTextures/assets/SurfaceImperfections003_1K_var1.jpg',
+    '/pages/GroundReflectionsAndVideoTextures/assets/SurfaceImperfections003_1K_Normal.jpg',
+  ]);
+
+  return (
+    <Reflector
+      blur={[400, 100]}
+      resolution={512}
+      args={[10, 10]}
+      mirror={0.5}
+      mixBlur={6}
+      mixStrength={1.5}
+      rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+    >
+      {(Material, props) => <Material color='#a0a0a0' metalness={0.4} roughness={floor} normalMap={normal} normalScale={[2, 2]} {...props} />}
+    </Reflector>
+  );
 };
