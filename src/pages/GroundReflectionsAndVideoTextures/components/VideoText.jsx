c908035e23e7e1fc9acc861a79890a60fc271a8d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-01-12 21:46:23 +0300

    finish init video text component

--- src/pages/GroundReflectionsAndVideoTextures/components/VideoText.jsx
+++ src/pages/GroundReflectionsAndVideoTextures/components/VideoText.jsx
@@ -1,3 +1,25 @@
-export const VideoText = () => {
-  return <></>;
+import { useEffect, useState } from 'react';
+import { sRGBEncoding } from 'three';
+import { Text } from '@react-three/drei';
+
+export const VideoText = (props) => {
+  const [video] = useState(() =>
+    Object.assign(document.createElement('video'), {
+      src: '/src/pages/GroundReflectionsAndVideoTextures/assets/drei.mp4',
+      crossOrigin: 'Anonymous',
+      loop: true,
+      muted: true,
+    })
+  );
+
+  useEffect(() => void video.play(), [video]);
+
+  return (
+    <Text font='/fonts/Inter-Bold.woff' fontSize={3} letterSpacing={-0.06} {...props}>
+      drei
+      <meshBasicMaterial toneMapped={false}>
+        <videoTexture attach='map' args={[video]} encoding={sRGBEncoding} />
+      </meshBasicMaterial>
+    </Text>
+  );
 };
